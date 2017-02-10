4fe579ad20f7b06b14a510658dc0c853389b84f7
iamyb <b20yang@outlook.com>
2017-02-10 17:10:50 +0800

    update perf.py

--- doc/perf.py
+++ doc/perf.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-#lines = [line.rstrip('\n') for line in open('test_performance_results.txt')]
 with open("test_performance_results.txt", "r") as f:
 	lines = []
 	for line in f:
@@ -12,24 +11,24 @@ with open("test_performance_results.txt", "r") as f:
 	glibc_memset = lines[3].split(', ')
 
 	size = [32,64,128,256,512,1024,2048,4096,8192,16384]
+	marker=[32,512,1024,2048,4096,8192,16384]
 
+        # Plot 1
 	plt.subplot(2, 1, 1)
 	plt.plot(size, lbb, marker='+', label='lbb_alloc_free')
 	plt.plot(size, glibc,  marker='.', label='glibc_malloc_free')
 	plt.ylabel('ns(avg) spent on alloc+free per block')
 	plt.xlabel('block size')
-
-	size1=[32,512,1024,2048,4096,8192,16384]
-	plt.xticks(size1)
+	plt.xticks(marker)
 	plt.legend()
 
+        # Plot 2
 	plt.subplot(2, 1, 2)
 	plt.plot(size, lbb_memset, marker='+', label='lbb_alloc_free')
 	plt.plot(size, glibc_memset,  marker='.', label='glibc_malloc_free')
 	plt.ylabel('ns(avg) spent on alloc+memset+free per block')
 	plt.xlabel('block size')
-
-	plt.xticks(size1)
+	plt.xticks(marker)
 	plt.legend()
 
 	fig = plt.gcf()
