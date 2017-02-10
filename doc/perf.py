52e4fedaf3d7d1147f05df0d5e46ce5a202e4704
iamyb <b20yang@outlook.com>
2017-02-10 13:59:11 +0800

    integrate perf.py with test_performance

--- doc/perf.py
+++ doc/perf.py
@@ -1,31 +1,37 @@
 import matplotlib.pyplot as plt
 
-## todo with the test_performance.c 
-size = [32,64,128,256,512,1024,2048,4096,8192,16384]
+#lines = [line.rstrip('\n') for line in open('test_performance_results.txt')]
+with open("test_performance_results.txt", "r") as f:
+	lines = []
+	for line in f:
+		lines.append(line.rstrip('\n'))	
+		
+	lbb          = lines[0].split(', ')
+	glibc        = lines[1].split(', ')
+	lbb_memset   = lines[2].split(', ')
+	glibc_memset = lines[3].split(', ')
 
-lbb  = [26,21,24,60,20,21,64,68,21,58]
-glibc= [129,101,151,147,137,142,1465,2676,3469,3789]
+	size = [32,64,128,256,512,1024,2048,4096,8192,16384]
 
-lbb_memset  =[26, 26, 33, 89, 139, 178, 314, 749, 1671, 3344]
-glibc_memset=[123,140,155,182,232,306,1524,3742,7707,17273]
+	plt.subplot(2, 1, 1)
+	plt.plot(size, lbb, marker='+', label='lbb_alloc_free')
+	plt.plot(size, glibc,  marker='.', label='glibc_malloc_free')
+	plt.ylabel('ns(avg) spent on alloc+free per block')
+	plt.xlabel('block size')
 
-plt.subplot(2, 1, 1)
-plt.plot(size, lbb, marker='+', label='lbb_alloc_free')
-plt.plot(size, glibc,  marker='.', label='glibc_malloc_free')
-plt.ylabel('ns(avg) spent on alloc+free per block')
-plt.xlabel('block size')
+	size1=[32,512,1024,2048,4096,8192,16384]
+	plt.xticks(size1)
+	plt.legend()
 
-size1=[32,512,1024,2048,4096,8192,16384]
-plt.xticks(size1)
-plt.legend()
+	plt.subplot(2, 1, 2)
+	plt.plot(size, lbb_memset, marker='+', label='lbb_alloc_free')
+	plt.plot(size, glibc_memset,  marker='.', label='glibc_malloc_free')
+	plt.ylabel('ns(avg) spent on alloc+memset+free per block')
+	plt.xlabel('block size')
 
-plt.subplot(2, 1, 2)
-plt.plot(size, lbb_memset, marker='+', label='lbb_alloc_free')
-plt.plot(size, glibc_memset,  marker='.', label='glibc_malloc_free')
-plt.ylabel('ns(avg) spent on alloc+memset+free per block')
-plt.xlabel('block size')
+	plt.xticks(size1)
+	plt.legend()
 
-plt.xticks(size1)
-plt.legend()
-
-plt.show()
+	fig = plt.gcf()
+	fig.set_size_inches(16, 9)
+	fig.savefig('performance.png', dpi=200)
